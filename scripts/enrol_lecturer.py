2dfb3bc11e6c1040b76063296657746957b7e525
Rajahni <crajahni@yahoo.com>
2023-05-02 09:40:48 -0500

    redid script logic

--- scripts/enrol_lecturer.py
+++ scripts/enrol_lecturer.py
@@ -23,11 +23,11 @@ with open('../Database/enrol_lecturer.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["enrol_id","lecturerid", "course_id"])
     enrol_id=1
-    for studentid in range(1,101):
+    for lecturerid in range(520000001,520000101):
 
         num_courses = random.randint(1,5)
         choices = random.sample(range(0, len(courses)), num_courses)
         course_choices = []
         for choice in choices:
-            writer.writerow([enrol_id,studentid+100000, courses[choice]])
+            writer.writerow([enrol_id,lecturerid, courses[choice]])
             enrol_id+=1
