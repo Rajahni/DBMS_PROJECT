5716633891df01c759841d83ffc3f053d8288f53
Rajahni <crajahni@yahoo.com>
2023-05-02 09:41:36 -0500

    should be all current scripts

--- scripts/updated_scripts.sql
+++ scripts/updated_scripts.sql
@@ -175,7 +175,7 @@ LOAD DATA INFILE '/var/lib/mysql-files/Lecturer_ID.csv'
 INTO TABLE Lecturer 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
+LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 -- Load data from 'Courses' into the 'Course' table
@@ -186,6 +186,56 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
+-- Load data from 'enrol_students.csv' into the 'Enrol_Student' table
+LOAD DATA INFILE '/var/lib/mysql-files/enrol_students.csv' 
+INTO TABLE Enrol_Student 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+-- Load data from 'enrol_lecturer.csv' into the 'Enrol_Lecturer' table
+LOAD DATA INFILE '/var/lib/mysql-files/enrol_lecturer.csv' 
+INTO TABLE Enrol_Lecturer 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+CREATE TRIGGER check_enrolment_limits
+BEFORE INSERT ON Enrol_Student
+FOR EACH ROW
+BEGIN
+    DECLARE course_count INT;
+    SELECT COUNT(*) INTO course_count
+    FROM Enrol_Student
+    WHERE student_id = NEW.student_id;
+    IF course_count >= 6 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: Maximum number of courses per student exceeded.';
+    ELSEIF course_count < 3 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: Minimum number of courses per student not reached.';
+    END IF;
+END;
+
+CREATE TRIGGER check_teaching_limits
+BEFORE INSERT ON Enrol_Lecturer
+FOR EACH ROW
+BEGIN
+    DECLARE course_count INT;
+    SELECT COUNT(*) INTO course_count
+    FROM Enrol_Lecturer
+    WHERE lecturerid = NEW.lecturerid;
+    IF course_count >= 5 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: Maximum number of courses per lecturer exceeded.';
+    ELSEIF course_count < 1 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Error: Minimum number of courses per lecturer not reached.';
+    END IF;
+END;
+
 CREATE TRIGGER check_course_members
 BEFORE INSERT ON Enrol_Student
 FOR EACH ROW
@@ -193,10 +243,8 @@ BEGIN
     DECLARE member_count INT;
     SELECT COUNT(*) INTO member_count
     FROM Enrol_Student
-    WHERE course_id = NEW.course_id;
-    IF member_count >= 10 THEN
-        
-    ELSE
+    WHERE courseid = NEW.courseid;
+    IF member_count < 10 THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Error: Course must have at least 10 members.';
     END IF;
